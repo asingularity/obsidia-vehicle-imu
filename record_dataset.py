b9dd59a25e5fba5e6e38acc1e7798eba517a8d4e
asingularity <csaba.petre@gmail.com>
2023-01-13 09:09:57 -0800

    untested: added LED, infinite record, timestamp to filename, range and rate for accelerometer

--- record_dataset.py
+++ record_dataset.py
@@ -4,7 +4,9 @@ from datetime import datetime
 
 import board
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
+from adafruit_lsm6ds import Rate, AccelRange, GyroRange
 import busio
+import digitalio
 
 from fps_counter import FPSCounter
 
@@ -13,33 +15,61 @@ def record_dataset():
     i2c = busio.I2C(board.SCL, board.SDA)
     sensor = LSM6DSOX(i2c)
 
+    # old:
+    #         self.redPin = 15
+    #         self.greenPin = 11
+    #         self.bluePin = 13
+
+    led_red = digitalio.DigitalInOut(board.D22)  # D22: pin 15
+    led_red.direction = digitalio.Direction.OUTPUT
+
+    led_green = digitalio.DigitalInOut(board.D17)  # D17: pin 11
+    led_green.direction = digitalio.Direction.OUTPUT
+
+    led_blue = digitalio.DigitalInOut(board.D27)  # D27: pin 13
+    led_blue.direction = digitalio.Direction.OUTPUT
+
+    led_red.value = True  # off
+    led_green.value = True  # off
+    led_blue.value = True  # off
+
+    sensor.accelerometer_range = AccelRange.RANGE_2G
+    sensor.accelerometer_data_rate = Rate.RATE_833_HZ
+
     fps = FPSCounter(params={'display_every_k_seconds': 1})
 
-    f = open('tmp_accel_gyro.txt', 'w')
+    timestamp = datetime.now().isoformat()
+    f = open('/home/csaba/datasets/' + timestamp + '_accelerometer.txt', 'w')
 
     print()
     print('starting recording...')
     print()
 
-    t0 = time.time()
-    while time.time() < t0 + 10:
+    last_led_time = time.time()
+
+    #t0 = time.time()
+    while True:  #time.time() < t0 + 10:
         #print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
         #print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
         #print("")
 
+        if time.time() - last_led_time > 0.5:
+            led_blue.value = not led_blue.value  # flash blue at 2 Hz
+
         timestamp = datetime.now().isoformat()
         accel = sensor.acceleration
-        gyro = sensor.gyro
-        f.write(timestamp + ', ' + str(accel[0]) + ', ' + str(accel[1]) + ', ' + str(accel[2]) + '\n')
+        # gyro = sensor.gyro
+        f.write(timestamp + ',' + str(accel[0]) + ',' + str(accel[1]) + ',' + str(accel[2]) + '\n')
 
         fps.update()
 
-    print('finished recording.')
-    print()
+    # Not used for infinite recording:
 
-    fps.update(force_display=True)
-    f.close()
-
+    # print('finished recording.')
+    # print()
+    #
+    # fps.update(force_display=True)
+    # f.close()
 
 
 if __name__ == '__main__':
