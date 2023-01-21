cbd4d4909514872e016a838373f4c19dae06fbb6
asingularity <csaba.petre@gmail.com>
2023-01-21 12:08:31 -0800

    time bounded ground truth events. session timestamps for ground truth file; separate ids for dataset files. ready to test.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # obsidia-vehicle-imu
 vehicle imu recording and real-time processing
+
+## recording datasets
+
+Turn on device in wifi proximity. Device should blink blue at 1 Hz.
+
+Go to car. 
+
+Press start/stop button to start a dataset. Device should blink blue at 4 Hz.
+
+Record data. 
+
+Press 1, 2, 3, 4 for starting a ground truth event. Device LED will blink red at 4 Hz. 
+
+Press ESC to end ground truth event. Device will go back to blinking blue at 4 Hz.
+
+Press start/stop button to stop dataset. Device should go back to blinking blue at 1 Hz.
+
+Ground truth events will be in one file over the whole recording session; datasets for accel and gyro will be divided by dataset id.
+Postprocessing will combine these correctly.
+
+Example:
+
--- record_dataset.py
+++ record_dataset.py
@@ -19,8 +19,8 @@ from parameters import DATASETS_FOLDER
 import keyboard
 
 
-DATASET_TIMESTAMP = datetime.now().isoformat()
-GROUND_TRUTH_FILE_NAME = os.path.join(DATASETS_FOLDER, DATASET_TIMESTAMP + '_ground_truth.txt')
+SESSION_TIMESTAMP = datetime.now().isoformat()
+GROUND_TRUTH_FILE_NAME = os.path.join(DATASETS_FOLDER, SESSION_TIMESTAMP + '_ground_truth.txt')
 F_GROUND_TRUTH = open(GROUND_TRUTH_FILE_NAME, 'w')
 F_GROUND_TRUTH.close()
 
@@ -28,33 +28,43 @@ LED_EVENT_START_TIME = time.time()
 
 START_BUTTON_PUSHED = False
 
+GROUND_TRUTH_EVENT_NOW = False
+
 
 # catch keyboard events for ground truth:
 def on_accelerate_event():
     # print()
     # print('ground truth: accelerate event!')
     # print()
+    global GROUND_TRUTH_EVENT_NOW
+    if not GROUND_TRUTH_EVENT_NOW:
+        timestamp = datetime.now().isoformat()
+        f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
+        f_g.write(timestamp + ' ' + 'accelerate' + '\n')
+        f_g.close()
 
-    timestamp = datetime.now().isoformat()
-    f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
-    f_g.write(timestamp + ' ' + 'accelerate' + '\n')
-    f_g.close()
+        global LED_EVENT_START_TIME
+        LED_EVENT_START_TIME = time.time()
 
-    global LED_EVENT_START_TIME
-    LED_EVENT_START_TIME = time.time()
+        GROUND_TRUTH_EVENT_NOW = True
+
 
 def on_turn_event():
     # print()
     # print('ground truth: turn event!')
     # print()
+    global GROUND_TRUTH_EVENT_NOW
+    if not GROUND_TRUTH_EVENT_NOW:
 
-    timestamp = datetime.now().isoformat()
-    f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
-    f_g.write(timestamp + ' ' + 'turn' + '\n')
-    f_g.close()
+        timestamp = datetime.now().isoformat()
+        f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
+        f_g.write(timestamp + ' ' + 'start turn' + '\n')
+        f_g.close()
 
-    global LED_EVENT_START_TIME
-    LED_EVENT_START_TIME = time.time()
+        global LED_EVENT_START_TIME
+        LED_EVENT_START_TIME = time.time()
+
+        GROUND_TRUTH_EVENT_NOW = True
 
 
 def on_break_event():
@@ -62,13 +72,18 @@ def on_break_event():
     # print('ground truth: break event!')
     # print()
 
-    timestamp = datetime.now().isoformat()
-    f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
-    f_g.write(timestamp + ' ' + 'break' + '\n')
-    f_g.close()
+    global GROUND_TRUTH_EVENT_NOW
+    if not GROUND_TRUTH_EVENT_NOW:
 
-    global LED_EVENT_START_TIME
-    LED_EVENT_START_TIME = time.time()
+        timestamp = datetime.now().isoformat()
+        f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
+        f_g.write(timestamp + ' ' + 'start break' + '\n')
+        f_g.close()
+
+        global LED_EVENT_START_TIME
+        LED_EVENT_START_TIME = time.time()
+
+        GROUND_TRUTH_EVENT_NOW = True
 
 
 def on_other_event():
@@ -76,13 +91,30 @@ def on_other_event():
     # print('ground truth: other event!')
     # print()
 
-    timestamp = datetime.now().isoformat()
-    f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
-    f_g.write(timestamp + ' ' + 'other' + '\n')
-    f_g.close()
+    global GROUND_TRUTH_EVENT_NOW
+    if not GROUND_TRUTH_EVENT_NOW:
 
-    global LED_EVENT_START_TIME
-    LED_EVENT_START_TIME = time.time()
+        timestamp = datetime.now().isoformat()
+        f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
+        f_g.write(timestamp + ' ' + 'start other' + '\n')
+        f_g.close()
+
+        global LED_EVENT_START_TIME
+        LED_EVENT_START_TIME = time.time()
+
+        GROUND_TRUTH_EVENT_NOW = True
+
+
+def on_stop_event():
+    global GROUND_TRUTH_EVENT_NOW
+    if GROUND_TRUTH_EVENT_NOW:
+
+        timestamp = datetime.now().isoformat()
+        f_g = open(GROUND_TRUTH_FILE_NAME, 'a')
+        f_g.write(timestamp + ' ' + 'end' + '\n')
+        f_g.close()
+
+        GROUND_TRUTH_EVENT_NOW = False
 
 
 # define the callback function
@@ -97,19 +129,21 @@ def on_keypress(e):
         on_break_event()
     if e.name == '4':
         on_other_event()
-    if e.name == 'space'
+    if e.name == 'esc':
+        on_stop_event()
+    if e.name == 'space':
         START_BUTTON_PUSHED = True
 
 
 keyboard.on_press(on_keypress, suppress=True)
 
 
-def record_dataset(sensor, led_red, led_green, led_blue):
+def record_dataset(sensor, led_red, led_green, led_blue, dataset_id):
 
     fps = FPSCounter(params={'display_every_k_seconds': 1})
 
-    f_a = open(os.path.join(DATASETS_FOLDER, DATASET_TIMESTAMP + '_accelerometer.txt'), 'w')
-    f_g = open(os.path.join(DATASETS_FOLDER, DATASET_TIMESTAMP + '_gyro.txt'), 'w')
+    f_a = open(os.path.join(DATASETS_FOLDER, SESSION_TIMESTAMP + '_' + str(dataset_id) + '_accelerometer.txt'), 'w')
+    f_g = open(os.path.join(DATASETS_FOLDER, SESSION_TIMESTAMP + '_' + str(dataset_id) + '_gyro.txt'), 'w')
 
     print()
     print('starting recording...')
@@ -126,13 +160,16 @@ def record_dataset(sensor, led_red, led_green, led_blue):
         #print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
         #print("")
 
-        if time.time() - LED_EVENT_START_TIME < 2.0:
-            led_blue.value = True
-            led_red.value = False
-        elif time.time() - last_led_time > 0.25:
-            led_red.value = True
-            led_blue.value = not led_blue.value  # flash blue at 2 Hz
-            last_led_time = time.time()
+        if GROUND_TRUTH_EVENT_NOW:
+            if time.time() - last_led_time > 0.25:
+                led_red.value = not led_red.value  # flash red at 2 Hz
+                led_blue.value = True
+                last_led_time = time.time()
+        else:
+            if time.time() - last_led_time > 0.25:
+                led_red.value = True
+                led_blue.value = not led_blue.value  # flash blue at 2 Hz
+                last_led_time = time.time()
 
         timestamp = datetime.now().isoformat()
         accel = sensor.acceleration
@@ -153,6 +190,7 @@ def record_dataset(sensor, led_red, led_green, led_blue):
     f_g.close()
 
     START_BUTTON_PUSHED = False
+
 
 def dataset_recorder_loop():
 
@@ -181,6 +219,7 @@ def dataset_recorder_loop():
     led_blue.value = True  # off
 
     last_led_time = time.time()
+    dataset_id = 0
 
     while True:
 
@@ -195,8 +234,8 @@ def dataset_recorder_loop():
             START_BUTTON_PUSHED = False
 
             # this function will listen for push of button again to end the recording:
-            record_dataset(sensor, led_red, led_green, led_blue)
-
+            record_dataset(sensor, led_red, led_green, led_blue, dataset_id)
+            dataset_id += 1
 
 
 if __name__ == '__main__':
