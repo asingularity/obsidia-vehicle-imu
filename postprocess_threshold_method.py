b5f3708e438d0aae99e4656a89987e038149b11b
asingularity <csaba.petre@gmail.com>
2023-01-31 16:06:00 -0800

    detect all events

--- postprocess_threshold_method.py
+++ postprocess_threshold_method.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 REF_TIMESTAMP = '2023-01-01T00:00:00.000000'
 
 ACC_THRESH = 4.4
+BRAKE_THRESH = 4.4
+CORNER_THRESH = 4.0
 
 
 def _detect_accel_events(acc_arr, timestamp_arr):
@@ -81,6 +83,197 @@ def _detect_accel_events(acc_arr, timestamp_arr):
     return detected_event_timestamps, detected_event_types
 
 
+def _detect_brake_events(acc_arr, timestamp_arr):
+    '''
+
+    An event will be logged if the acceleration exceeds the threshold for at least 100ms.
+    If the event lasts uninterrupted (stays above the threshold - 17% hysteresis) for more than 100ms, a single event is logged.
+    A second event is logged if the acceleration drops below the threshold - 17% hysteresis for 100ms and then exceeds the threshold again for 100ms.
+
+    Defaults: acceleration and braking 4.4m/s^2. Cornering 5.5m/s^2. These are set high to log a minimum number of events on most vehicles.
+    Testing: artificially low to generate events. Acceleration and braking 3.0m/s^2. Cornering 3.5m/s^2
+    In practice: choose levels between the defaults and the testing levels.
+
+    :param acc_arr:
+    :param timestamp_arr:
+    :return:
+    '''
+
+    # types:
+    #   1 for acceleration
+    #   0 for end
+
+    event_ongoing = False
+    event_start_t = 0
+
+    last_event_stop_t = -np.inf
+
+    new_event_allowed = True
+
+    detected_event_timestamps = []
+    detected_event_types = []
+
+    for k in range(len(acc_arr)):
+        t = timestamp_arr[k]
+        acc = acc_arr[k]
+
+        if event_ongoing:
+            if acc > -BRAKE_THRESH + 0.17 * BRAKE_THRESH:
+                if t - event_start_t > 0.1:
+                    detected_event_timestamps.append(event_start_t)
+                    detected_event_types.append(1)
+
+                    detected_event_timestamps.append(t)
+                    detected_event_types.append(0)
+
+                    last_event_stop_t = t
+                    new_event_allowed = False
+                else:
+                    pass
+
+                event_ongoing = False
+
+        elif not new_event_allowed:
+            if acc < -BRAKE_THRESH + 0.17 * BRAKE_THRESH:
+                last_event_stop_t = t
+            if t - last_event_stop_t > 0.1:
+                new_event_allowed = True
+        else:
+            if acc < -BRAKE_THRESH and new_event_allowed:
+
+                event_ongoing = True
+                event_start_t = t
+
+    detected_event_timestamps = np.array(detected_event_timestamps)
+    detected_event_types = np.array(detected_event_types)
+
+    return detected_event_timestamps, detected_event_types
+
+
+def _detect_corner_events(acc_arr, timestamp_arr):
+    '''
+
+    An event will be logged if the acceleration exceeds the threshold for at least 100ms.
+    If the event lasts uninterrupted (stays above the threshold - 17% hysteresis) for more than 100ms, a single event is logged.
+    A second event is logged if the acceleration drops below the threshold - 17% hysteresis for 100ms and then exceeds the threshold again for 100ms.
+
+    Defaults: acceleration and braking 4.4m/s^2. Cornering 5.5m/s^2. These are set high to log a minimum number of events on most vehicles.
+    Testing: artificially low to generate events. Acceleration and braking 3.0m/s^2. Cornering 3.5m/s^2
+    In practice: choose levels between the defaults and the testing levels.
+
+    :param acc_arr:
+    :param timestamp_arr:
+    :return:
+    '''
+
+    # types:
+    #   1 for acceleration
+    #   0 for end
+
+    acc_arr_abs = np.abs(acc_arr)
+
+    event_ongoing = False
+    event_start_t = 0
+
+    last_event_stop_t = -np.inf
+
+    new_event_allowed = True
+
+    detected_event_timestamps = []
+    detected_event_types = []
+
+    for k in range(len(acc_arr_abs)):
+        t = timestamp_arr[k]
+        acc = acc_arr_abs[k]
+
+        if event_ongoing:
+            if acc < CORNER_THRESH - 0.17 * CORNER_THRESH:
+                if t - event_start_t > 0.1:
+                    detected_event_timestamps.append(event_start_t)
+                    detected_event_types.append(1)
+
+                    detected_event_timestamps.append(t)
+                    detected_event_types.append(0)
+
+                    last_event_stop_t = t
+                    new_event_allowed = False
+                else:
+                    pass
+
+                event_ongoing = False
+
+        elif not new_event_allowed:
+            if acc > CORNER_THRESH - 0.17 * CORNER_THRESH:
+                last_event_stop_t = t
+            if t - last_event_stop_t > 0.1:
+                new_event_allowed = True
+        else:
+            if acc > CORNER_THRESH and new_event_allowed:
+
+                event_ongoing = True
+                event_start_t = t
+
+    detected_event_timestamps = np.array(detected_event_timestamps)
+    detected_event_types = np.array(detected_event_types)
+
+    return detected_event_timestamps, detected_event_types
+
+
+def _generate_graph(exp_path, dataset_name, session_timestamp, dataset_event_types, timestamp_arr_meas, values_arr_meas, timestamp_arr_gt, events_arr_gt, values_label, detection_label, detected_event_timestamps, detected_event_types, thresh_plot):
+    '''
+
+    :param timestamp_arr:
+    :param values_arr:
+    :param values_label:
+    :param detection_label:
+    :param detected_event_timestamps:
+    :param detected_event_types:
+    :return:
+    '''
+
+
+
+    num_subplots = 3
+    fig_bar, ax = plt.subplots(nrows=num_subplots, ncols=1, sharex=True, figsize=(40, 20))
+
+    ax[0].cla()
+    ax[0].plot(timestamp_arr_meas, values_arr_meas, 'r.')
+    ax[0].get_xaxis().get_major_formatter().set_scientific(False)
+    ax[0].get_yaxis().get_major_formatter().set_scientific(False)
+    ax[0].axhline(y=thresh_plot, color='b')
+    ax[0].set_title(dataset_name + ' ' + values_label + ', with detected events')
+
+    ax[1].cla()
+    ax[1].plot(timestamp_arr_meas, values_arr_meas, 'r.')
+    ax[1].get_xaxis().get_major_formatter().set_scientific(False)
+    ax[1].get_yaxis().get_major_formatter().set_scientific(False)
+    ax[1].set_title(dataset_name + ' ' + values_label + ', with ground truth events')
+
+    ax[2].cla()
+    try:
+        ax[2].plot(timestamp_arr_gt, events_arr_gt, 'ko')
+    except:
+        print()
+        print('timestamp_arr_gt', timestamp_arr_gt)
+        print('events_arr', events_arr_gt)
+        print()
+        raise
+
+    ax[2].get_xaxis().get_major_formatter().set_scientific(False)
+    ax[2].get_yaxis().get_major_formatter().set_scientific(False)
+    ax[2].set_title(dataset_name + ' ' + 'ground truth events start: ' + str(dataset_event_types))
+
+    for k in range(len(events_arr_gt)):
+        event_t = timestamp_arr_gt[k]
+        ax[1].axvline(x=event_t, color='g')
+
+    for k in range(len(detected_event_timestamps)):
+        event_t = detected_event_timestamps[k]
+        ax[0].axvline(x=event_t, color='g')
+
+    fig_bar.savefig(os.path.join(exp_path + "/" + dataset_name + "_" + session_timestamp + "_" + detection_label + "_detect.png"), dpi=100)
+
+
 def process_dataset(session_timestamp, dataset_event_types, dataset_name, exp_path):
 
     dataset_index_str = '0'
@@ -90,51 +283,60 @@ def process_dataset(session_timestamp, dataset_event_types, dataset_name, exp_pa
     timestamp_arr_acc, acc_x_arr, acc_y_arr, acc_z_arr, acc_abs_arr = _load_acc_file(filename=os.path.join(DATASETS_FOLDER, session_timestamp + '_' + dataset_index_str + '_accelerometer.txt'))
     timestamp_arr_gyro, gyro_x_arr, gyro_y_arr, gyro_z_arr, gyro_abs_arr = _load_acc_file(filename=os.path.join(DATASETS_FOLDER, session_timestamp + '_' + dataset_index_str + '_gyro.txt'))
 
-    timestamp_arr_gt, events_arr = _load_ground_truth_file(filename=os.path.join(DATASETS_FOLDER, session_timestamp + '_ground_truth.txt'), event_types=dataset_event_types)
-
-    num_subplots = 3
-    fig_bar, ax = plt.subplots(nrows=num_subplots, ncols=1, sharex=True, figsize=(40, 20))
-
-    ax[0].cla()
-    ax[0].plot(timestamp_arr_acc, acc_x_arr, 'r.')
-    ax[0].get_xaxis().get_major_formatter().set_scientific(False)
-    ax[0].get_yaxis().get_major_formatter().set_scientific(False)
-    ax[0].axhline(y=ACC_THRESH, color='b')
-    ax[0].set_title(dataset_name + ' ' + 'accelerometer X, with detected events')
-
-    ax[1].cla()
-    ax[1].plot(timestamp_arr_acc, acc_x_arr, 'r.')
-    ax[1].get_xaxis().get_major_formatter().set_scientific(False)
-    ax[1].get_yaxis().get_major_formatter().set_scientific(False)
-    # ax[1].axhline(y=ACC_THRESH, color='b')
-    ax[1].set_title(dataset_name + ' ' + 'accelerometer X, with ground truth events')
-
-    ax[2].cla()
-    try:
-        ax[2].plot(timestamp_arr_gt, events_arr, 'ko')
-    except:
-        print()
-        print('timestamp_arr_gt', timestamp_arr_gt)
-        print('events_arr', events_arr)
-        print()
-        raise
-
-    ax[2].get_xaxis().get_major_formatter().set_scientific(False)
-    ax[2].get_yaxis().get_major_formatter().set_scientific(False)
-    ax[2].set_title(dataset_name + ' ' + 'ground truth events start: ' + str(dataset_event_types))
-
-    for k in range(len(events_arr)):
-        event_t = timestamp_arr_gt[k]
-        ax[1].axvline(x=event_t, color='g')
+    timestamp_arr_gt, events_arr_gt = _load_ground_truth_file(filename=os.path.join(DATASETS_FOLDER, session_timestamp + '_ground_truth.txt'), event_types=dataset_event_types)
 
     # detect events
     detected_event_timestamps, detected_event_types = _detect_accel_events(acc_arr=acc_x_arr, timestamp_arr=timestamp_arr_acc)
 
-    for k in range(len(detected_event_timestamps)):
-        event_t = detected_event_timestamps[k]
-        ax[0].axvline(x=event_t, color='g')
+    _generate_graph(exp_path=exp_path,
+                    dataset_name=dataset_name,
+                    session_timestamp=session_timestamp,
+                    dataset_event_types=dataset_event_types,
+                    timestamp_arr_meas=timestamp_arr_acc,
+                    values_arr_meas=acc_x_arr,
+                    timestamp_arr_gt=timestamp_arr_gt,
+                    events_arr_gt=events_arr_gt,
+                    values_label='accelerometer X',
+                    detection_label='acceleration',
+                    detected_event_timestamps=detected_event_timestamps,
+                    detected_event_types=detected_event_types,
+                    thresh_plot=ACC_THRESH)
 
-    fig_bar.savefig(os.path.join(exp_path + "/" + dataset_name + "_accelerometer_data.png"), dpi=100)
+    # detect events
+    detected_event_timestamps, detected_event_types = _detect_brake_events(acc_arr=acc_x_arr, timestamp_arr=timestamp_arr_acc)
+
+    _generate_graph(exp_path=exp_path,
+                    dataset_name=dataset_name,
+                    session_timestamp=session_timestamp,
+                    dataset_event_types=dataset_event_types,
+                    timestamp_arr_meas=timestamp_arr_acc,
+                    values_arr_meas=acc_x_arr,
+                    timestamp_arr_gt=timestamp_arr_gt,
+                    events_arr_gt=events_arr_gt,
+                    values_label='accelerometer X',
+                    detection_label='braking',
+                    detected_event_timestamps=detected_event_timestamps,
+                    detected_event_types=detected_event_types,
+                    thresh_plot=-BRAKE_THRESH)
+
+    # detect events
+    detected_event_timestamps, detected_event_types = _detect_corner_events(acc_arr=acc_y_arr, timestamp_arr=timestamp_arr_acc)
+
+    _generate_graph(exp_path=exp_path,
+                    dataset_name=dataset_name,
+                    session_timestamp=session_timestamp,
+                    dataset_event_types=dataset_event_types,
+                    timestamp_arr_meas=timestamp_arr_acc,
+                    values_arr_meas=acc_y_arr,
+                    timestamp_arr_gt=timestamp_arr_gt,
+                    events_arr_gt=events_arr_gt,
+                    values_label='accelerometer Y',
+                    detection_label='cornering',
+                    detected_event_timestamps=detected_event_timestamps,
+                    detected_event_types=detected_event_types,
+                    thresh_plot=-CORNER_THRESH)
+
+
 
 
 def main():
