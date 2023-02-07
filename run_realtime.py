9cee94cfbff7d2ff6d0d64545b12cb1e795315ed
asingularity <csaba.petre@gmail.com>
2023-02-07 13:34:12 -0800

    attempt again

--- run_realtime.py
+++ run_realtime.py
@@ -57,7 +57,7 @@ def realtime_loop(sensor, led_red, led_green, led_blue):
 
     mean_accel = np.zeros(3)
     mean_gyro = np.zeros(3)
-    mean_tau = 0.999
+    mean_tau = 0.9
 
     fps = FPSCounter(params={'display_every_k_seconds': 2})
 
@@ -94,13 +94,13 @@ def realtime_loop(sensor, led_red, led_green, led_blue):
                 else:
                     alert_now = 'Normal Driving'
 
-                accel_scaled = [0, 0, 0]
+                accel_scaled = [0, 0, 9.8]
                 gyro_scaled = [0, 0, 0]
 
                 for k in range(3):
-                    accel_scaled[k] = (accel[k] - mean_accel[k]) / mean_accel[k]
-                    gyro_scaled[k] = (gyro[k] - mean_gyro[k]) / mean_gyro[k]
-                    
+                    accel_scaled[k] = accel_scaled[k] + max(min(1.0, mean_accel[k]), -1.0)
+                    gyro_scaled[k] = max(min(1.0, mean_gyro[k]), -1.0)
+
                 requests.put(url + '/' + json.dumps({'bar1': accel_scaled[0], 'bar2': accel_scaled[1], 'bar3': accel_scaled[2],
                                                      'bar4': gyro_scaled[0], 'bar5': gyro_scaled[1], 'bar6': gyro_scaled[2],
                                                      'alert': alert_now}))
